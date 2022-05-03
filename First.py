e2d9bcc1822122538c473808764fc8368b758847
Allyssa Kate Maranan <59193566+allymaranan@users.noreply.github.com>
2022-05-04 00:33:08 +0800

    Add files via upload

---
+++ First.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "First.ipynb",
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "e3Eyg2EnuXm3",
+        "outputId": "d4762119-76be-4a8f-9a7c-03909946f9ef"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "1\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(1)"
+      ]
+    }
+  ]
+}
